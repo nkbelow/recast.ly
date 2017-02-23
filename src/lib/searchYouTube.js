c4f20a6ccb9bb0be6c10faa3ae220d3c13de3fed
nkbelow <nkbelow@gmail.com>
2017-02-23 11:59:48 -0800

    Passed searchYoutube tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    type: 'GET',
+    data: {
+      type: 'video',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      videoEmbeddable: 'true'
+    },
+    dataType: 'json',
+    timeout: 5000,
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    success: (data) => {
+      callback(data.items);
+    },
+  });
 };
 
 window.searchYouTube = searchYouTube;
